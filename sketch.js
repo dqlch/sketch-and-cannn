cd878a24c6291a493658adb0287e01e0bcd8cbef
dqlch <146040651+dqlch@users.noreply.github.com>
2024-07-10 18:07:51 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+var backgroundImg;
+var tower;
+var canvas;
+var angle;
+function preload(){
+  backgroundImg = loadImage("./assets/background.gif");
+}
+
+function setup() {
+  //createCanvas(400,400);
+  canvas = createCanvas(1200,600);
+
+  engine = Engine.create();
+  world = engine.world;
+  angle = -PI / 4;
+  tower = new Tower(150,350,160,310);
+  cannon = new Cannon(180, 130, 70, 50, angle);
+
+
+ // rectMode(CENTER);
+  //ellipseMode(RADIUS);
+}
+
+function draw() 
+{
+  background(51);
+  image(backgroundImg, 0, 0, width, height);
+  Engine.update(engine);
+
+  tower.display();
+  cannon.display();
+
+ 
+}
+
